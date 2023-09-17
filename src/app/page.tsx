9e2225ecf5f39981bf3c015a20d928b782de7053
ddfilipov <denisfilipov12+3@gmail.com>
2023-09-17 15:44:12 +0200

    finally loading pokemon on server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { GetServerSideProps } from "next";
 import { Fragment } from "react";
 
-export default function Home() {
+export default async function Home() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const data = await res.json();
+    console.log("what's happening??", JSON.stringify(data));
     return <div>CHILDREN PASSED TO LAYOUT</div>;
 }
--- src/atomic/organism/mainArea.tsx
+++ src/atomic/organism/mainArea.tsx
@@ -1,12 +1,20 @@
 import { FC } from "react";
 import styled from "styled-components";
 
+interface MainAreaProps {
+    pokemon: any;
+}
+
 const Container = styled.div`
     border: 1px solid white;
 `;
 
 const MainArea: FC = () => {
-    return <Container>Main Area</Container>;
+    return (
+        <Container>
+            <h2>Pokémon List</h2>
+        </Container>
+    );
 };
 
 export default MainArea;
