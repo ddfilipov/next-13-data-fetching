b55163c937d1034ad23b90c445d706f619a938ad
ddfilipov <denisfilipov12+3@gmail.com>
2023-09-17 16:57:00 +0200

    footer and header styled

--- src/atomic/organism/footer.tsx
+++ src/atomic/organism/footer.tsx
@@ -2,11 +2,20 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+    display: flex;
+    place-content: center;
     border: 1px solid white;
+    text-align: center;
+    width: 100%;
+    height: 100%;
 `;
 
 const Footer: FC = () => {
-    return <Container>FOOTER</Container>;
+    return (
+        <Container>
+            <h1>FOOTER</h1>
+        </Container>
+    );
 };
 
 export default Footer;
--- src/atomic/organism/header.tsx
+++ src/atomic/organism/header.tsx
@@ -2,11 +2,20 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+    display: flex;
+    place-content: center;
     border: 1px solid white;
+    text-align: center;
+    width: 100%;
+    height: 100%;
 `;
 
 const Header: FC = () => {
-    return <Container>HEADER</Container>;
+    return (
+        <Container>
+            <h1>HEADER</h1>
+        </Container>
+    );
 };
 
 export default Header;
