19a31e121c3051cdc0d16e579f47274300fc009d
ddfilipov <denisfilipov12+3@gmail.com>
2023-09-17 16:16:12 +0200

    fixed use client stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import MainArea from "@/atomic/organism/mainArea";
 import { GetServerSideProps } from "next";
 import { Fragment } from "react";
 
 export default async function Home() {
     const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
     const data = await res.json();
-    console.log("what's happening??", JSON.stringify(data));
-    return <div>CHILDREN PASSED TO LAYOUT</div>;
+    console.log("what's happening??", JSON.stringify(data.results));
+    return <MainArea pokemon={data.results} />;
 }
--- src/atomic/organism/mainArea.tsx
+++ src/atomic/organism/mainArea.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC } from "react";
 import styled from "styled-components";
 
@@ -9,10 +10,15 @@ const Container = styled.div`
     border: 1px solid white;
 `;
 
-const MainArea: FC = () => {
+const MainArea: FC<MainAreaProps> = ({ pokemon }) => {
     return (
         <Container>
             <h2>Pokémon List</h2>
+            <ul>
+                {pokemon.map((poke: { name: string; url: string }) => {
+                    return <li key={poke.name}>{poke.name}</li>;
+                })}
+            </ul>
         </Container>
     );
 };
